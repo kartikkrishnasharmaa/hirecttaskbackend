c7557da2f8af4409f52c91471da9b1fb8c771c9f
kartikkrishnasharmaa <kartikkrishnasharmaa@gmail.com>
2023-10-11 08:28:02 +0530

    first commi changet

---
+++ models/Task.js
@@ -0,0 +1,23 @@
+const mongoose = require( "mongoose" );
+
+const taskSchema = new mongoose.Schema( {
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+}, {
+  timestamps: true
+} );
+
+
+const Task = mongoose.model( "Task", taskSchema );
+module.exports = Task;
